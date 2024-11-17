d5ad9354643a60b677733b9fed551c6dee6e9c60
System Administrator <root@Howards-MacBook-Air.local>
2024-11-17 13:50:06 -0800

    Fixing FormsModule

--- sampleProjectTwo/src/app/app.module.ts
+++ sampleProjectTwo/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SampleComponentComponent } from './sample-component/sample-component.component';
+import { SampleComponentTwoComponent } from './sample-component-two/sample-component-two.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SampleComponentComponent
+    SampleComponentComponent,
+    SampleComponentTwoComponent
   ],
   imports: [
     BrowserModule,
---
+++ sampleProjectTwo/src/app/sample-component-two/sample-component-two.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sample-component-two',
+  templateUrl: './sample-component-two.component.html',
+  styleUrl: './sample-component-two.component.scss'
+})
+export class SampleComponentTwoComponent {
+
+}
